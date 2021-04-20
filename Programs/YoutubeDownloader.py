ed101cd821bb7952052448452c9b4b60749fc0e2
Joe Griffin <josgriffin@joegrifnslaptop.attlocal.net>
2021-04-19 23:15:26 -0500

    Adding Programs Folder

---
+++ Programs/YoutubeDownloader.py
@@ -0,0 +1,12 @@
+# Testing Work
+
+from pytube import YouTube
+
+print("Enter URL of Video")
+link = input(": ")
+
+video = YouTube(link)
+stream = video.streams.get_highest_resolution()
+
+stream.download("/Users/josgriffin/Downloads")
+print('Saved "' + stream.title + '"')
