ba90aa0904b66afcb690cabce9546b661a577fda
Joseph Griffin <hoopsman16@aol.com>
2023-12-08 15:38:54 -0600

    Added Png File
    added wordle icon to wordle project

--- Programs/haskell_class_work/Haskell_In_Class_Two_Joe_Griffin.hs
+++ Programs/haskell_class_work/Haskell_In_Class_Two_Joe_Griffin.hs
@@ -131,7 +131,6 @@ evalAll :: [Expr] -> [Int]
 evalAll [] = []
 evalAll (x : xs) = head (eval x) : evalAll xs
 
--- Replit AI also helped me to construct this defintion specifically
 -- Replit AI also helped me to construct this defintion specifically 
 mode :: Ord a => [a] -> a
 mode = head . maximumBy (compare `on` length) . group . sort
