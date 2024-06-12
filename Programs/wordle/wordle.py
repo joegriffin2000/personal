8581aaf6ed813d55e1191efff5b761e4135f5083
Joseph Griffin <joseph.griffin@valkyrie.ai>
2024-06-12 13:44:06 -0500

    added commenting mostly

--- Programs/wordle/wordle.py
+++ Programs/wordle/wordle.py
@@ -1,14 +1,15 @@
 #wordle dupe
 #by dynaj03
-
 import random
 
+#loading list of all possible solution words
 file = "savefile1.txt"
 f = open(file,"r")
 ListOfWords = f.read().split()
 f.close()
 ListOfWords.sort()
 
+#loading list of all possible guess words
 file = "sgb-words.txt"
 f = open(file,"r")
 AllWordsStanford = f.read().split()
@@ -17,6 +18,7 @@ AllWordsStanford.sort()
 
 AllLetters = set(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'])
 
+#The game loop
 def main():
     RandomWord = ListOfWords[random.randint(0,len(ListOfWords))]
     print("Enter a Word! You have 6 Tries.")
@@ -48,7 +50,8 @@ def main():
         print("Victory!")
     else:
         print("Too Bad...")
-        
+
+#checking to see which characters match up and which charactsers dont match but appear in the word        
 def getMatchString(RandomWord,userWord):
     matchString=["*","*","*","*","*"]
     for i in range(0,len(RandomWord)):
@@ -68,24 +71,25 @@ def copyCheck():
         print("ERROR: WORD IS IN THE LIST MORE THAN ONCE ALREADY")   
     else:
         print(False)
-        
+
+#checking to see all plural words that exist in the list of possible solution words     
 def printingPlurals():
     for i in ListOfWords:
         if i.startswith("d") and i.endswith('s'):
             print(i)
 
+#getting the total number of words
 def wordCount():
-    count=0
-    for i in ListOfWords:
-        count +=1
-    print(count)
+    print(len(ListOfWords))
 
+#finding all the words that have a certain ending (has been modified before for certain beginning of words too)
 def pieceCheck():
     s1 = 'ed'
     for i in ListOfWords:
         if ((i.endswith(s1))):
             print(i)
-    
+
+#Running everything from here. Don't uncomment functions unless debugging
 if __name__ == "__main__":
     main()
     #copyCheck()
